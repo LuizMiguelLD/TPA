c82a79d699434c574032f21e31aa4a55adaa5f58
LuizMiguelLD <migueldavidlo2005@gmail.com>
2025-11-11 17:17:22 -0300

    Add files via upload

---
+++ prjAulaVetores12/Exercicio5.java
@@ -0,0 +1,30 @@
+package prjAulaRemotaVetores;
+
+import java.util.Scanner;
+
+public class Exercicio5 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		final int TAM=10;
+		int a[], i, j;
+		a = new int[TAM];
+		
+		for(i=0; i<TAM; i++) {
+			System.out.println("Digite o "+(i+1)+"º valor de A");
+			a[i] = ler.nextInt();
+		}
+		
+		for(i=0; i<TAM; i++) {
+			System.out.println("Relação dos divisores até o número "+a[i]);
+			for(j=1; j<=a[i] ; j++) {
+				if (a[i]%j==0) {
+					System.out.println(j);
+				}
+			}
+		}
+		
+		ler.close();
+	}
+
+}
